2a1a5db557b5b0ba74b768f2b21e3d18f82d05a2
akshat0808 <59463847+akshat0808@users.noreply.github.com>
2020-02-29 17:43:09 +0530

    gift2

---
+++ app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { DisplayComponent } from './display/display.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactComponent,
+    AboutComponent,
+    DisplayComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
